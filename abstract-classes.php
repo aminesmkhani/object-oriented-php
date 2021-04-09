ad450d58c44a713c95eb5a918254c6e89aea55fa
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-04-09 23:14:09 +0430

    abstract class

--- abstract-classes.php
+++ abstract-classes.php
@@ -1,1 +1,45 @@
 <?php
+
+
+class AchievementType
+{
+    public function name()
+    {
+        $class = (new ReflectionClass($this))->getShortName();
+
+        return trim(preg_replace('/[A_Z]/',' $0',$class));
+    }
+
+
+    public function icone()
+    {
+       return strtolower(str_replace(' ','-',$this->name())).'.png';
+    }
+}
+
+
+class FirstThousandPoints extends AchievementType
+{
+
+    public function qualifier()
+    {
+
+    }
+
+}
+
+class FirstBestAnswer extends AchievementType
+{
+
+    public function qualifier()
+    {
+
+    }
+
+}
+
+
+
+$achievement = new  FirstBestAnswer();
+
+echo $achievement->name();
