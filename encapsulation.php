d600d521d32924b6fa5f4805a657ab00530826c7
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-04-13 10:28:48 +0430

    Encapsulation

---
+++ encapsulation.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class TennisMatch
+{
+
+    protected $playerOne;
+
+    public function score()
+    {
+        // is there a winner
+        // does someone have an advantage
+        // are they in deuce
+    }
+
+
+    protected function hasWinner()
+    {
+        
+    }
+
+    protected function hasAdvantage()
+    {
+        
+    }
+
+    protected function inDeuce()
+    {
+        
+    }
+}
